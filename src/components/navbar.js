9adf199642c9123211c646a5835102a536498b70
Mirco Grillo <mirco.grillomg@gmail.com>
2019-05-22 22:37:02 +0200

    Adds contacts page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default () => (
-  <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+  <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
     <Navbar.Brand href="/">
       <img
         src="https://s3-eu-west-1.amazonaws.com/wood-sound-festival/LogoWood.svg"
