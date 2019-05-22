b02c21d76fac7499404c13d0a9e028f89d7c6d51
Mirco Grillo <mirco.grillomg@gmail.com>
2019-05-22 10:19:24 +0200

    Refactors floating component

--- src/components/floating.js
+++ src/components/floating.js
@@ -3,19 +3,13 @@ import React from "react";
 export default () => (
   <article style={{ display: "flex", justifyContent: "center" }}>
     <img
-      src="https://mixtape.select-themes.com/wp-content/uploads/2016/09/new-album-slider-vector-color.png"
+      src="https://s3-eu-west-1.amazonaws.com/wood-sound-festival/bubbles.png"
       height="600"
       class="bubbles"
       alt=""
     />
     <img
-      src="https://mixtape.select-themes.com/wp-content/uploads/2016/09/new-album-slider-vector-graphic.png"
-      height="600"
-      class="bubbles graphic"
-      alt=""
-    />
-    <img
-      src="https://mixtape.select-themes.com/wp-content/uploads/2016/09/new-album-slider-head.png"
+      src="https://s3-eu-west-1.amazonaws.com/wood-sound-festival/head.png"
       class="head"
       alt=""
     />
