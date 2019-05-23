7af0a14f3ff8e8c0f7ed2a44aa07fc7fc7c012cd
Mirco Grillo <mirco.grillomg@gmail.com>
2019-05-23 14:56:14 +0200

    Test pWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   plugins: [
-    `gatsby-plugin-favicon`
+    `gatsby-plugin-favicon`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Wood Sound Festival`,
+        short_name: `WSF`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/favicon.png`,
+      },
+    },
   ],
 }
