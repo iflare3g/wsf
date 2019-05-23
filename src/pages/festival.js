70c3d086abc64ea61356aa8d48ba260a6d98ac1e
Mirco Grillo <mirco.grillomg@gmail.com>
2019-05-23 10:15:03 +0200

    HOTFIX bumps v0.0.1beta2

--- src/pages/festival.js
+++ src/pages/festival.js
@@ -54,15 +54,7 @@ export default () => (
             Il Festival
           </h1>
           <p className="mt-3 text-justify pr-lg-5 pr-0 wsf-grey font-weight-light">
-            Nel Comune di Nemoli (PZ), nel Parco Nazionale dell’Appennino
-            Lucano, è presente una gemma preziosa che la natura ha regalato agli
-            abitanti della Basilicata: il Lago Sirino, un piccolo bacino idrico
-            naturale della Basilicata posto alle falde della vetta omonima ad
-            un’altitudine di 788 metri sul livello del mare. Sulle sue rive si
-            sviluppano varie iniziative turistiche e sorgono numerosi esercizi
-            commerciali raggiungibili in breve tempo. Lungo il perimetro del
-            lago è presente una stradina di 1 km indicata come ciclostrada;
-            nelle immediate vicinanze si può usufruire anche di un’area camper.{" "}
+            La serata sarà arricchita da numerosi artisti che si sostituiranno sul palco variando generi e stili musicali. La line up è in continua evoluzione, ogni aggiornamento potete trovarlo sui nostri canali social. Il WSF è alla sua seconda edizione, con il successo del 2017 e la partecipazione della Main Guest GAZZELLE oltre a tutta la line up composta da: Umberto Scaramozza, I Bytecore, Raffaele Pelosi, Benny Sola, Antonio Viceconte e Massimo Cantisani, lo staff è riuscito a portare a termine una splendida serata superando le loro stesse aspettative. Con l’edizione del 2018 l’organizzazione vuole ripetersi nell’impresa, proponendo una serata all’insegna di buona musica, offrendo cibo e bevande di qualità e fornendo un pregevole intrattenimento nell’accogliente area del “Boschetto del Lago Sirino”. Il WSF non sarà solo musica, ma una celebrazione dell’arte in tutte le sue forme, una vera attività di promozione del territorio e una valida occasione di condivisione e aggregazione giovanile.
           </p>
         </Col>
       </Row>
