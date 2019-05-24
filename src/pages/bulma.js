aab0cddb8481d67a57b1d764ef6dbd8cc35c4331
Mirco Grillo <mirco.grillomg@gmail.com>
2019-05-24 16:53:17 +0200

    Tries bulma

---
+++ src/pages/bulma.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default () => (
+<React.Fragment>
+  <section className="hero is-medium is-primary is-bold">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">
+          Contatti
+        </h1>
+        <h2 className="subtitle">
+          Primary bold subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
+  <section className="container">
+    <div className="columns is-variable">
+      <div className="column">
+        First column
+      </div>
+      <div className="column">
+        Second column
+      </div>
+      <div className="column">
+        Third column
+      </div>
+      <div className="column">
+        Fourth column
+      </div>
+    </div>
+  </section>
+</React.Fragment>
+)
