aeec6d09fde8dc4d27dead88cf7fc08f1bfabfcf
Mirco Grillo <mirco.grillomg@gmail.com>
2019-06-27 16:46:01 +0200

    Re-adds countdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,14 @@ import Floating from "../components/floating";
 import StyledArtists from "../components/artists";
 import StyledSpotify from "../components/lazyspotify";
 import StyledFirstMay from "../components/firstmay";
+import Countdown from "../components/countdown";
 import "../styles/artists.scss";
 
 export default () => (
   <React.Fragment>
     <Layout>
       <Floating />
+      <Countdown />
     </Layout>
     <StyledArtists className="section hero container-fluid artists-row" />
     <StyledSpotify className="section hero container-fluid artists-row" />
