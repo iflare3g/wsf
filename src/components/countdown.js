cc288b763b92d2b50a6ab06a1c96f76fba3c2e62
Mirco Grillo <mirco.grillomg@gmail.com>
2019-08-11 21:43:58 +0200

    bumps v0.0.6

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -4,7 +4,21 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Countdown from "react-countdown-now";
 
-const Completionist = () => <span>Time to have fun!</span>;
+const Completionist = () => (
+  <Container className="py-lg-5 py-3">
+    <Row className="text-center" style={{ fontSize: "3rem" }}>
+      <Col lg="12">
+        <h1 className="wsf-grey is-size-1">Fulminacci</h1>
+        <h5 className="wsf-red has-text-weight-bold is-size-1 animated pulse infinite">
+          It's time!
+        </h5>
+        <h5 className="wsf-red has-text-weight-bold is-size-1 animated pulse infinite">
+          Enjoy your Wood Sound Festival!
+        </h5>
+      </Col>
+    </Row>
+  </Container>
+);
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
     // Render a completed state
